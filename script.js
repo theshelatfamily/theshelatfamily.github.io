2dd4f3f2fa7d3fef40b23574c6fc4938ef5a6f79
Nishir <nshelat@usc.edu>
2017-01-24 12:15:53 -0800

    Begin fades

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ $( "#enterButton" ).click(function() {
         scrollTop: $("#profiles").offset().top},
         'slow');
 });
+
+$( "#kPic" ).click(function() {
+	$( "#kPic" ).fadeTo( "slow", 1 );
+
+	$( "#mPic" ).fadeTo( "slow", 0.6 );
+	$( "#nPic" ).fadeTo( "slow", 0.6 );
+	$( "#starPic" ).fadeTo( "slow", 0.6 );
+	$( "#sPic" ).fadeTo( "slow", 0.6 );
+
+	$( "#mDescription" ).fadeOut( "slow" );
+	$( "#nDescription" ).fadeOut( "slow" );
+	$( "#starDescription" ).fadeOut( "slow" );
+	$( "#sDescription" ).fadeOut( "slow" );
+
+	$( "#kDescription" ).delay(800).fadeTo( "slow", 1 );
+});
+
+$( "#mPic" ).click(function() {
+	$( "#mPic" ).fadeTo( "slow", 1 );
+
+	$( "#kPic" ).fadeTo( "slow", 0.6 );
+	$( "#nPic" ).fadeTo( "slow", 0.6 );
+	$( "#starPic" ).fadeTo( "slow", 0.6 );
+	$( "#sPic" ).fadeTo( "slow", 0.6 );
+
+	$( "#kDescription" ).fadeOut( "slow" );
+	$( "#nDescription" ).fadeOut( "slow" );
+	$( "#starDescription" ).fadeOut( "slow" );
+	$( "#sDescription" ).fadeOut( "slow" );
+
+	$( "#mDescription" ).delay(800).fadeTo( "slow", 1 );
+});
